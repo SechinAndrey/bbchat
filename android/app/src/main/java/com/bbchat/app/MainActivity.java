dc5d5680fccaa387d556b83ca2a5d242a15c84c7
Andrey Sechin <sechinandreyv@gmail.com>
2025-04-01 21:46:20 +0300

    add capacitor with android app

---
+++ android/app/src/main/java/com/bbchat/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bbchat.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bbchat.app',
+  appName: 'BbChat',
+  webDir: 'dist'
+};
+
+export default config;
