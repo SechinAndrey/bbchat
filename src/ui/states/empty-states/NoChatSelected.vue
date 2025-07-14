2c7db097269e556212de438edee9f8bd1e25250a
Andrey Sechin <sechinandreyv@gmail.com>
2025-07-14 15:08:28 +0300

    refactor: improve NoChatSelected

--- src/ui/states/empty-states/NoChatSelected.vue
+++ src/ui/states/empty-states/NoChatSelected.vue
@@ -1,21 +1,28 @@
 <script setup lang="ts">
-import { ChatBubbleOvalLeftIcon } from "@heroicons/vue/24/solid";
+import { ChatBubbleLeftRightIcon } from "@heroicons/vue/24/outline";
+
+const props = withDefaults(
+  defineProps<{
+    title: string;
+    text: string;
+  }>(),
+  {
+    title: "Чат не вибрано",
+    text: "Виберіть розмову з меню розмов.",
+  },
+);
 </script>
 
 <template>
   <div class="h-full flex flex-col justify-center items-center">
     <div
-      class="w-10 h-10 mr-4 mb-5 flex justify-center items-center rounded-full bg-gray-50 dark:bg-gray-700 transition duration-500"
+      class="w-10 h-10 mr-4 mb-5 flex justify-center items-center rounded-full bg-secondary transition duration-500"
     >
-      <ChatBubbleOvalLeftIcon
-        class="w-7 h-7 text-gray-400 dark:text-white dark:opacity-70"
-      />
+      <ChatBubbleLeftRightIcon class="w-7 h-7 text-secondary-active" />
     </div>
 
-    <p class="heading-2 text-color mb-3">No chat selected</p>
+    <p class="heading-2 text-color mb-3">{{ props.title }}</p>
 
-    <p class="body-2 text-color flex">
-      Select a conversation from the conversation menu.
-    </p>
+    <p class="body-2 text-color flex">{{ props.text }}</p>
   </div>
 </template>
