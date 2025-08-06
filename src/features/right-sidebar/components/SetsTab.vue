c477c408727846fd1c2eb76b36afc36be60e86b0
Andrey Sechin <sechinandreyv@gmail.com>
2025-08-06 18:49:36 +0300

    feat: SetsTab makeup

--- src/features/right-sidebar/components/SetsTab.vue
+++ src/features/right-sidebar/components/SetsTab.vue
@@ -7,6 +7,8 @@ import type { EntityType } from "@src/shared/types/common";
 
 // components
 import Spinner from "@src/ui/states/loading-states/Spinner.vue";
+import EmptyState from "@src/ui/states/empty-states/EmptyState.vue";
+import { RectangleStackIcon, TrashIcon } from "@heroicons/vue/24/outline";
 
 const selectionsStore = useSelectionsStore();
 const entity = inject("entity") as EntityType;
@@ -17,7 +19,6 @@ selectionsStore.fetchSelections(entity, id);
 
 <template>
   <div class="p-4">
-    <h3 class="text-lg font-semibold mb-4">Підбірки</h3>
     <div v-if="selectionsStore.isLoading" class="flex justify-center pt-10">
       <Spinner />
     </div>
@@ -26,29 +27,58 @@ selectionsStore.fetchSelections(entity, id);
         <div
           v-for="selection in selectionsStore.selections"
           :key="selection.id"
+          class="relative"
         >
-          <div class="flex items-center justify-between">
-            <div>ID</div>
-            <div>{{ selection.id }}</div>
+          <button
+            class="absolute right-1 p-2 hover:bg-secondary rounded-[4px] text-secondary-active"
+          >
+            <TrashIcon class="h-5 w-5">Remove</TrashIcon>
+          </button>
+          <div class="flex">
+            <div class="text-theme-t-alt text-[0.813rem] min-w-[49%]">ID</div>
+            <button class="text-[0.813rem] underline">
+              {{ selection.id }}
+            </button>
           </div>
-          <div class="flex items-center justify-between">
-            <div>Type</div>
-            <div>{{ selection.type.name }}</div>
+          <div class="flex">
+            <div class="text-theme-t-alt text-[0.813rem] min-w-[49%]">Type</div>
+            <div class="text-[0.813rem]">{{ selection.type.name }}</div>
           </div>
-          <div class="flex items-center justify-between">
-            <div>Date of Creation</div>
-            <div>{{ formatConversationDate(selection.created_at) }}</div>
+          <div class="flex">
+            <div class="text-theme-t-alt text-[0.813rem] min-w-[49%]">
+              Дата створення
+            </div>
+            <div class="text-[0.813rem]">
+              {{ formatConversationDate(selection.created_at) }}
+            </div>
           </div>
-          <div class="flex items-center justify-between">
-            <div>Manager</div>
-            <div>{{ selection?.manager?.name || "Unknown" }}</div>
+          <div class="flex">
+            <div class="text-theme-t-alt text-[0.813rem] min-w-[49%]">
+              Менеджер
+            </div>
+            <div class="text-[0.813rem]">
+              {{ selection?.manager?.name || "<Невідомо>" }}
+            </div>
           </div>
+          <div class="flex">
+            <div class="text-theme-t-alt text-[0.813rem] min-w-[49%]">
+              Кількість площин
+            </div>
+            <div class="text-[0.813rem]">
+              {{ selection?.boards_count || "<Невідомо>" }}
+            </div>
+          </div>
+
+          <hr class="my-4" />
         </div>
       </div>
 
-      <div v-else class="text-gray-500">
-        Немає підбірок для цього {{ entity }}.
-      </div>
+      <EmptyState
+        v-else
+        :icon="RectangleStackIcon"
+        title="Пібірок немає"
+        class="bg-theme-bg py-5 rounded"
+      />
     </div>
   </div>
 </template>
