eaaf5f830d06c9cc3b1197ff0677b7b22ce1883c
Andrey Sechin <sechinandreyv@gmail.com>
2025-11-11 22:31:43 +0200

    feat: add android app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "ua.com.billboards.chat",
-  appName: "BbChat",
+  appName: "Billboards Комунікації",
   webDir: "dist",
   plugins: {
     PushNotifications: {
