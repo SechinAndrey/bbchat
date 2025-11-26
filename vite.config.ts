95377a1708d3c94df284510d91ae52ac653f9171
Andrey Sechin <sechinandreyv@gmail.com>
2025-11-26 16:02:24 +0200

    feat: Добавить PWA WS-257910/18700430/21099555

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import alias from "@rollup/plugin-alias";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 const rootDir = resolve(__dirname);
 
@@ -10,7 +11,90 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    plugins: [vue(), alias()],
+    plugins: [
+      vue(),
+      alias(),
+      VitePWA({
+        registerType: "autoUpdate",
+        devOptions: {
+          enabled: true,
+        },
+        workbox: {
+          globIgnores: ["**/firebase-messaging-sw.js"],
+          runtimeCaching: [
+            // todo: investigate caching strategies
+          ],
+        },
+        manifest: {
+          name: "Billboards Комунікації",
+          short_name: "BB Chat",
+          description: "Усі канали зв'язку Billboards в одному місці",
+          theme_color: "#3d445c",
+          background_color: "#ffffff",
+          display: "standalone",
+          orientation: "portrait",
+          scope: "/",
+          start_url: "/",
+          icons: [
+            {
+              src: "pwa-logos/pwa-logo-48.png",
+              sizes: "48x48",
+              type: "image/png",
+            },
+            {
+              src: "pwa-logos/pwa-logo-72.png",
+              sizes: "72x72",
+              type: "image/png",
+            },
+            {
+              src: "pwa-logos/pwa-logo-96.png",
+              sizes: "96x96",
+              type: "image/png",
+            },
+            {
+              src: "pwa-logos/pwa-logo-120.png",
+              sizes: "120x120",
+              type: "image/png",
+            },
+            {
+              src: "pwa-logos/pwa-logo-128.png",
+              sizes: "128x128",
+              type: "image/png",
+            },
+            {
+              src: "pwa-logos/pwa-logo-144.png",
+              sizes: "144x144",
+              type: "image/png",
+            },
+            {
+              src: "pwa-logos/pwa-logo-152.png",
+              sizes: "152x152",
+              type: "image/png",
+            },
+            {
+              src: "pwa-logos/pwa-logo-180.png",
+              sizes: "180x180",
+              type: "image/png",
+            },
+            {
+              src: "pwa-logos/pwa-logo-192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-logos/pwa-logo-256.png",
+              sizes: "256x256",
+              type: "image/png",
+            },
+            {
+              src: "pwa-logos/pwa-logo-512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
+    ],
     resolve: {
       alias: {
         "@src": resolve(rootDir, "src"),
